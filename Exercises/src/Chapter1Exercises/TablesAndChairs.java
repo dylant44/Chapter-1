5f88bca8e7dacaf8be12c52480ccc0d4b429906c
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-08-27 09:02:35 -0500

    Added Hekko World

---
+++ Exercises/src/Chapter1Exercises/TablesAndChairs.java
@@ -0,0 +1,10 @@
+package Chapter1Exercises;
+
+public class TablesAndChairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
