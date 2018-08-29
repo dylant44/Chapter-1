b66fd4af79087d3a42ce8076fa3350f3a18fda26
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-08-29 09:30:49 -0500

    integer demo

---
+++ Chapter2/src/youDoIt/dataDemo.java
@@ -0,0 +1,16 @@
+package youDoIt;
+
+public class dataDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int aWholeNumber = 315;
+		final int NUMBER_OF_STATES = 50;
+		
+		System.out.print("The number is ");
+		System.out.println(aWholeNumber);
+		System.out.println("The number of states is " + NUMBER_OF_STATES);
+	}
+
+}
