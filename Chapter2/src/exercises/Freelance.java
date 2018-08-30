5876dde08295e6ece4a40b5f3462b754879e7452
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-08-30 09:54:29 -0500

    freelance project

---
+++ Chapter2/src/exercises/Freelance.java
@@ -0,0 +1,36 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Freelance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int int1 = 10;
+		int int2 = 12;
+		int intTotal = int1 + int2;
+		//String name;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter an integer >> ");
+		int1 = input.nextInt();
+		System.out.println("Please enter another integer >> ");
+		int2 = input.nextInt();
+		
+		
+		
+		
+		System.out.println("The total is " + intTotal);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
