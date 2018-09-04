f1d54f21a20da7196726d06387555cb0b1e8614f
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-09-04 09:53:57 -0500

    quarts to gallons

---
+++ Chapter2/src/youDoIt/QuartsToGallons.java
@@ -0,0 +1,25 @@
+package youDoIt;
+
+import java.util.Scanner;
+
+public class QuartsToGallons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String quarts;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("how many quarts do you need?");
+		quarts = input.next();
+		
+		
+		
+		System.out.println("You will need " + quarts + " and ");
+		
+		
+		
+	}
+
+}
