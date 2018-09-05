738f4e0318fa312566a50194f4a9da67759cd7e4
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-09-05 09:49:30 -0500

    Feet and inches

---
+++ Chapter2/src/youDoIt/InchesToFeet.java
@@ -0,0 +1,28 @@
+package youDoIt;
+
+public class InchesToFeet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int inches = 50;
+		int inchesInFeet = 12;
+		int inchesResult;
+		int feetResult;
+		
+		
+		feetResult = (inches / inchesInFeet);
+		inchesResult = (inches % inchesInFeet);
+		
+		
+		System.out.print("The result is " + feetResult + "feet and " + inchesResult + "inches");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
