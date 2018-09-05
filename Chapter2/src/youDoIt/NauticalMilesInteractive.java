e8485c283d1579301551203f009721627785fd2b
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-09-05 09:40:41 -0500

    Nautical Miles Interactive

--- Chapter2/src/youDoIt/NauticalMilesInteractive.java
+++ Chapter2/src/youDoIt/NauticalMilesInteractive.java
@@ -1,13 +1,20 @@
 package youDoIt;
 
-public class NauticalMiles {
+import javax.swing.JOptionPane;
+
+public class NauticalMilesInteractive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		double KM_IN_NM = 1.852;
 		double M_IN_NM = 1.150779;
-		double NauticalMiles = 5;
+		double NauticalMiles;
+		String nautMilesString;
+		
+		nautMilesString = JOptionPane.showInputDialog(null, "How many Nautical miles?");
+		NauticalMiles = Integer.parseInt(nautMilesString);
+		
 		
 		System.out.print("The result in miles is " + (NauticalMiles * M_IN_NM));
 		System.out.print(" and the result in kilometers is " + (NauticalMiles * KM_IN_NM));
