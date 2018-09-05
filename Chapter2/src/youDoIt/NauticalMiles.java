4f1c45099180e499ff82cff12678af368fe482be
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-09-05 09:34:48 -0500

    Nautical miles

---
+++ Chapter2/src/youDoIt/NauticalMiles.java
@@ -0,0 +1,20 @@
+package youDoIt;
+
+public class NauticalMiles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double KM_IN_NM = 1.852;
+		double M_IN_NM = 1.150779;
+		double NauticalMiles = 5;
+		
+		System.out.print("The result in miles is " + (NauticalMiles * M_IN_NM));
+		System.out.print(" and the result in kilometers is " + (NauticalMiles * KM_IN_NM));
+		
+		
+		
+		
+	}
+
+}
