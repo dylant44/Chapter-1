8a3f30a097de26e93369d2e899e7f57ee805dad3
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-09-07 09:35:55 -0500

    eggs

--- Chapter2/src/youDoIt/Eggs.java
+++ Chapter2/src/youDoIt/Eggs.java
@@ -1,10 +1,44 @@
 package youDoIt;
 
+import javax.swing.JOptionPane;
+
 public class Eggs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int numDozen;
+		int numSingleEggs;
+		int numEggsNeeded;
+		double dozenPrice = 3.25;
+		double singleEggPrice = .45;
+		double totalPrice;
+		String eggsNeeded;
+		
+		
+		eggsNeeded = JOptionPane.showInputDialog("How many eggs do you need?");
+		numEggsNeeded = Integer.parseInt(eggsNeeded);
+		
+		numDozen = (numEggsNeeded / 12);
+		numSingleEggs = (numEggsNeeded % 12);
+		totalPrice = ((numDozen * dozenPrice) + (numSingleEggs * singleEggPrice));
+		
+		
+		
+		
+		System.out.print("You ordered " + numEggsNeeded + " eggs. That's " + numDozen + " dozen at $3.25 per dozen and " + numSingleEggs + " loose eggs at $0.45 each for a total of " + totalPrice);
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
