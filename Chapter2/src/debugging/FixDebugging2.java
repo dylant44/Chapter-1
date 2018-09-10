955ab81601b02ca3a8c31ceff35ab79dc8b04a95
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-09-10 08:56:43 -0500

    debugging

---
+++ Chapter2/src/debugging/FixDebugging2.java
@@ -0,0 +1,21 @@
+package debugging;
+
+public class FixDebugging2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int a, b;
+	      a = 7;
+	      b = 4;
+	      System.out.println("The sum is " + (a + b));
+	      System.out.println("The difference is " + (a - b));
+	      System.out.println("The product is " + (a * b));
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Chapter2/src/debugging/FixDebugging3.java
@@ -0,0 +1,21 @@
+package debugging;
+
+public class FixDebugging3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 99, b = 8, result;
+	      int c = 777777777;
+	      result = a % b;
+	      System.out.println("Divide " + a + " by " + b);
+	      System.out.println("remainder is " + result);
+	      System.out.print("c is a very large number: ");
+	      System.out.println(c);
+		
+		
+		
+		
+	}
+
+}
